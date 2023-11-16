49fdf909bff98affa5d2cbe7ed888e199a3d659a
murilomunari <129402165+murilomunari@users.noreply.github.com>
2023-11-16 09:44:26 -0300

    base do projeto feito

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,7 +1,40 @@
 package br.com.fiap;
 
+import br.com.fiap.infra.cors.CORSFilter;
+import br.com.fiap.infra.ConnectionFactory;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
 public class Main {
+
+    private static final String BASE_URI = "http://localhost:8080/";
+
+    public static HttpServer startServer() {
+
+        final ResourceConfig rc = new ResourceConfig()
+                // Configure container response filters (CORSFilter)
+                .register(CORSFilter.class)
+                // Configure ConnectionFactory
+                .register(ConnectionFactory.build())
+                // Configure os pacotes em que temos Recursos da API REST
+                .packages("br.com.fiap.domain.resource");
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var server = startServer();
+        System.out.println(String.format(
+                "started with endpoints available " +
+                        "as %s%nHit Ctrl-C to stop it....", BASE_URI));
+        try {
+            System.in.read();
+            server.stop();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
